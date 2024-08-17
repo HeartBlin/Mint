31a35e4574c0ccef7a3a5389a74797c40b15765a
HeartBlin <161874560+HeartBlin@users.noreply.github.com>
2024-08-17 23:04:30 +0300

    home/modules: Actually install the right element

--- home/modules/element/config.nix
+++ home/modules/element/config.nix
@@ -4,4 +4,4 @@ let
   inherit (lib) mkIf;
 
   cfg = config.Ark.element;
-in { config = mkIf cfg.enable { home.packages = [ pkgs.element ]; }; }
+in { config = mkIf cfg.enable { home.packages = [ pkgs.element-desktop ]; }; }

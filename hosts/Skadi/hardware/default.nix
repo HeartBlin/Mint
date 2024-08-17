31749a073d890895471c1b582becf0c9df6b464d
HeartBlin <161874560+HeartBlin@users.noreply.github.com>
2024-08-17 18:53:52 +0300

    host/Skadi/hardware: Underclokc & undervolt

--- hosts/Skadi/hardware/default.nix
+++ hosts/Skadi/hardware/default.nix
@@ -1,7 +1,33 @@
-{ config, lib, modulesPath, ... }:
+{ config, lib, modulesPath, pkgs, ... }:
 
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  # Underclock & Undervolt
+  # Until I go get my laptop fixed I'll sacrifice performance for nice temps
+  # Normally pushing 98C in high loads
+  hardware.cpu.x86.msr.enable =  true;
+  environment.systemPackages = with pkgs; [
+    linuxKernel.packages.linux_zen.cpupower
+    amdctl
+  ];
+  systemd.services.underclockAndUndervolt = let
+    cpupower = "${pkgs.linuxKernel.packages.linux_zen.cpupower}/bin/cpupower";
+    amdctl = "${pkgs.amdctl}/bin/amdctl";
+    bash = "${pkgs.bash}/bin/bash -c";
+  in {
+    description = "Underclocks & Undervolts CPU";
+
+    wantedBy = [ "multi-user.target" "post-resume.target" ];
+    after = [ "post-resume.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      Restart = "no";
+      ExecStart =
+        "${bash} '${cpupower} frequency-set -u 4.0Ghz && ${amdctl} -m -p0 -v 60'"; # 1175 mV
+    };
+  };
 
   boot = {
     initrd = {
@@ -10,6 +36,7 @@
         [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
     };
 
+    kernelPackages = pkgs.linuxKernel.packages.linux_zen;
     kernelModules = [ "kvm-amd" ];
     extraModulePackages = [ ];
 

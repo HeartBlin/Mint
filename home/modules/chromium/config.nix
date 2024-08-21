2bb52c0481665a2bb3b6fa1f66a63930424f2c0d
HeartBlin <161874560+HeartBlin@users.noreply.github.com>
2024-08-21 19:52:37 +0300

    chromium: Removed declarative extensions

--- home/modules/chromium/config.nix
+++ home/modules/chromium/config.nix
@@ -3,43 +3,12 @@
 let
   inherit (lib) mkIf;
 
-  createChromiumExtensionFor = x:
-    { id, sha256, version, }: {
-      inherit id;
-      crxPath = builtins.fetchurl {
-        url =
-          "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=${x}&x=id%3D${id}%26installsource%3Dondemand%26uc";
-        name = "${id}.crx";
-        inherit sha256;
-      };
-      inherit version;
-    };
-
-  createChromiumExtension = createChromiumExtensionFor
-    (lib.versions.major pkgs.ungoogled-chromium.version);
-
   cfg = config.Ark.chromium;
   hyprlandOnNvidia = osConfig.Ark.nvidia.enable && config.Ark.hyprland.enable;
 in {
   config = mkIf cfg.enable {
     programs.chromium = {
       enable = true;
-
-      extensions = [
-        # BitWarden
-        (createChromiumExtension {
-          id = "nngceckbapebfimnlniiiahkandclblb";
-          sha256 = "1s9h1zipfz2yrv90hjsnnxcrrv08vm20nhs1bpg9ninpgi5vmvw0";
-          version = "2024.7.1";
-        })
-
-        # uBlock Origin Lite
-        (createChromiumExtension {
-          id = "ddkjiahejlhfcafbddmgiahcphecmpfh";
-          sha256 = "14krvrwfjy168m9g8arisnx3fcjg7ih7zk178vymk12461ap0dyn";
-          version = "2024.8.19.905";
-        })
-      ];
 
       package = pkgs.ungoogled-chromium.override {
         commandLineArgs = [

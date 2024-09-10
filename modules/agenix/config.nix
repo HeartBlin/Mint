1bd1381d6d47767f33f62c425446621ad651a7be
HeartBlin <161874560+HeartBlin@users.noreply.github.com>
2024-09-10 17:51:22 +0000

    secrets: Fixed .age files

--- modules/agenix/config.nix
+++ modules/agenix/config.nix
@@ -5,28 +5,24 @@
       path = "/home/${username}/.ssh/GitHubAuth";
       owner = "${username}";
       mode = "600";
-      symlink = false;
     };
     GitHubAuthPub = {
       file = ../../secrets/GitHubAuthPub.age;
       path = "/home/${username}/.ssh/GitHubAuth.pub";
       owner = "${username}";
       mode = "644";
-      symlink = false;
     };
     GitHubSign = {
       file = ../../secrets/GitHubSign.age;
       path = "/home/${username}/.ssh/GitHubSign";
       owner = "${username}";
       mode = "600";
-      symlink = false;
     };
     GitHubSignPub = {
       file = ../../secrets/GitHubSignPub.age;
       path = "/home/${username}/.ssh/GitHubSign.pub";
       owner = "${username}";
       mode = "644";
-      symlink = false;
     };
   };
 }

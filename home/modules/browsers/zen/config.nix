12429205f982544595d0fafa9f5a4bb2ca96cd83
HeartBlin <161874560+HeartBlin@users.noreply.github.com>
2024-09-17 17:09:20 +0000

    feat: Latest version of Zen browser

--- home/modules/browsers/zen/config.nix
+++ home/modules/browsers/zen/config.nix
@@ -5,10 +5,20 @@
   ...
 }: let
   inherit (lib) mkIf;
+  version = "1.0.1-a.1";
 
   cfg = config.Ark.browsers.zen;
 in {
   config = mkIf cfg.enable {
-    home.packages = [inputs'.zen-browser.packages.specific];
+    home.packages = [
+      (inputs'.zen-browser.packages.specific.overrideAttrs
+        (x: {
+          inherit version;
+          src = builtins.fetchTarball {
+            url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
+            sha256 = "0ckd2ra2clhly8s0xk11ni7k1mnw0l9y061zp4h9xyxf4gkz898f";
+          };
+        }))
+    ];
   };
 }

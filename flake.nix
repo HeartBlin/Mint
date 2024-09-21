7725c0c1295134884c7ebf9d8c7e44c0cd61e2b8
HeartBlin <161874560+HeartBlin@users.noreply.github.com>
2024-09-21 21:08:34 +0000

    chore(flake): Revamp of inputs
    Verbose, alphbetical & followed inputs

--- flake.nix
+++ flake.nix
@@ -1,55 +1,113 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-
-    # Declarative disk partitioning
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Declarative home
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Secrets management
-    agenix.url = "github:ryantm/agenix";
-
-    # Widgets
-    ags.url = "github:Aylur/ags";
-
-    # CachyOS kernel provider
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-
-    # Wayland compositor
-    hyprland = {
-      type = "git";
-      submodules = true;
-      url = "https://github.com/hyprwm/Hyprland";
-      ref = "refs/tags/v0.43.0";
-    };
-
-    # SecureBoot support
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.1";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Get fullscreen transparency on foot
-    nyxexprs.url = "github:NotAShelf/nyxexprs";
-
-    # Zen browser
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
-  };
-
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ ./parts ];
       systems = [
-        "x86_64-linux" # "aarch64-linux"
+        "x86_64-linux" # Also add "aarch64-linux" when I have the hardware
       ];
     };
+
+  inputs = {
+    agenix = {
+      type = "git";
+      url = "https://github.com/ryantm/agenix";
+      submodules = false;
+
+      inputs = {
+        home-manager.follows = "home-manager";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    ags = {
+      type = "git";
+      url = "https://github.com/Aylur/ags";
+      submodules = false;
+
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    chaotic = {
+      type = "git";
+      url = "https://github.com/chaotic-cx/nyx";
+      ref = "nyxpkgs-unstable";
+      submodules = false;
+
+      inputs = {
+        home-manager.follows = "home-manager";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    disko = {
+      type = "git";
+      url = "https://github.com/nix-community/disko";
+      submodules = false;
+
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-parts = {
+      type = "git";
+      url = "https://github.com/hercules-ci/flake-parts";
+      submodules = false;
+
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      type = "git";
+      url = "https://github.com/nix-community/home-manager";
+      submodules = false;
+
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland = {
+      type = "git";
+      url = "https://github.com/hyprwm/Hyprland";
+      ref = "refs/tags/v0.43.0";
+      submodules = true;
+
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    lanzaboote = {
+      type = "git";
+      url = "https://github.com/nix-community/lanzaboote";
+      ref = "refs/tags/v0.4.1";
+      submodules = false;
+
+      inputs = {
+        flake-parts.follows = "flake-parts";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    nixpkgs = {
+      type = "git";
+      url = "https://github.com/NixOS/nixpkgs";
+      ref = "nixos-unstable";
+      submodules = false;
+    };
+
+    nyxexprs = {
+      type = "git";
+      url = "https://github.com/NotAShelf/nyxexprs";
+      submodules = false;
+
+      inputs = {
+        flake-parts.follows = "flake-parts";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    zen-browser = {
+      type = "git";
+      url = "https://github.com/MarceColl/zen-browser-flake";
+      submodules = false;
+
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 }

1755f58b3d45910b255235324d8a08c8b40a30b9
HeartBlin <161874560+HeartBlin@users.noreply.github.com>
2024-09-30 21:22:57 +0000

    pre-commit-hooks: Added devShell

--- parts/devShells.nix
+++ parts/devShells.nix
@@ -1,14 +1,22 @@
+{ inputs, ... }:
+
 {
-  perSystem = { pkgs, ... }: {
+  imports = [ inputs.pre-commit-hooks.flakeModule ];
+
+  perSystem = { config, pkgs, ... }: {
     devShells = {
       bun = pkgs.mkShellNoCC {
         nativeBuildInputs = with pkgs; [ bun fish ];
         shellHook = ''exec fish -C "echo Entered Bun dev shell."'';
       };
 
-      nix = pkgs.mkShellNoCC {
-        nativeBuildInputs = with pkgs; [ statix deadnix nixfmt-classic fish ];
-        shellHook = ''exec fish -C "echo Entered NixOS dev shell."'';
+      default = pkgs.mkShellNoCC {
+        buildInputs = with pkgs;
+          [ fish ] ++ config.pre-commit.settings.enabledPackages;
+        shellHook = ''
+          ${config.pre-commit.installationScript}
+          exec fish -C "echo Entered Nix dev shell."
+        '';
       };
     };
   };

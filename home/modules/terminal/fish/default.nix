7ec0cf88dc12c4c243adfa24bf652ed6508b67c8
HeartBlin <161874560+HeartBlin@users.noreply.github.com>
2024-10-01 20:40:13 +0000

    fish: Added direnv

--- home/modules/terminal/fish/default.nix
+++ home/modules/terminal/fish/default.nix
@@ -7,51 +7,58 @@ let
   cfg = config.Ark.terminal;
 in {
   config = mkIf (cfg.shell == "fish") {
-    programs.fish = {
-      enable = true;
+    programs = {
+      fish = {
+        enable = true;
 
-      interactiveShellInit = ''
-        set fish_greeting
-        set -gx FLAKE ${flakeDir}
+        interactiveShellInit = ''
+          set fish_greeting
+          set -gx FLAKE ${flakeDir}
 
-        function starship_transient_prompt_func
-          starship module character
-        end
+          function starship_transient_prompt_func
+            starship module character
+          end
 
-        starship init fish | source
-        enable_transience
-      '';
+          starship init fish | source
+          enable_transience
+        '';
 
-      functions = {
-        ".".body = ''nix shell nixpkgs#$argv[1] --command "fish"'';
-        ",".body = ''nix develop ${flakeDir}/.#$argv[1] --command "fish"'';
-        "fish_command_not_found".body = "echo Did not find command: $argv[1]";
-        "mkcd".body = "mkdir -p $argv && cd $argv";
-      };
-
-      shellAliases = {
-        ls = "${pkgs.eza}/bin/eza -l";
-        rebuild = "nh os switch";
-        boot = "nh os boot";
-        update = "nix flake update --flake ${flakeDir} && nh os switch";
-        clean = "nh clean all && nh os boot";
-      };
-    };
-
-    programs.starship = {
-      enable = true;
-      enableFishIntegration = false; # I do it manually
-
-      settings = {
-        add_newline = false;
-
-        character = {
-          disabled = false;
-          success_symbol = "[λ](bold purple)";
-          error_symbol = "[λ](bold red)";
+        functions = {
+          ".".body = ''nix shell nixpkgs#$argv[1] --command "fish"'';
+          ",".body = ''nix develop ${flakeDir}/.#$argv[1] --command "fish"'';
+          "fish_command_not_found".body = "echo Did not find command: $argv[1]";
+          "mkcd".body = "mkdir -p $argv && cd $argv";
         };
 
-        directory.disabled = false;
+        shellAliases = {
+          ls = "${pkgs.eza}/bin/eza -l";
+          rebuild = "nh os switch";
+          boot = "nh os boot";
+          update = "nix flake update --flake ${flakeDir} && nh os switch";
+          clean = "nh clean all && nh os boot";
+        };
+      };
+
+      starship = {
+        enable = true;
+        enableFishIntegration = false; # I do it manually
+
+        settings = {
+          add_newline = false;
+
+          character = {
+            disabled = false;
+            success_symbol = "[λ](bold purple)";
+            error_symbol = "[λ](bold red)";
+          };
+
+          directory.disabled = false;
+        };
+      };
+
+      direnv = {
+        enable = true;
+        silent = true;
       };
     };
   };

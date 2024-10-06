bbbf0917c0e5d0bd6f32a1be3a75857a65eab5ef
HeartBlin <heartblin@gmail.com>
2024-10-06 20:15:38 +0000

    feat: Reduced Nix eval time
    By following inputs that are alreay declared in the flake
    This does cause compilations to run, mainly the Hyprland one
    but im OK with it

--- flake.nix
+++ flake.nix
@@ -8,8 +8,12 @@
   inputs = {
     ##### Inputs meant to be followed #####
 
+    # Input for non-flake systems
+    flake-compat.url = "github:edolstra/flake-compat";
+
     # Flake framework
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
     # The main NixOS repo
     nixpkgs.url = "github:NixOS/nixpkgs";
@@ -21,26 +25,37 @@
 
     # Disk partitioning
     disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
 
     # Home management
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Wayland Compositor
     hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.inputs = {
+      nixpkgs.follows = "nixpkgs";
+      systems.follows = "systems";
+    };
 
     # SecureBoot support
     lanzaboote.url = "github:/nix-community/lanzaboote/v0.4.1";
+    lanzaboote.inputs = {
+      flake-compat.follows = "flake-compat";
+      flake-parts.follows = "flake-parts";
+      nixpkgs.follows = "nixpkgs";
+      pre-commit-hooks-nix.follows = "pre-commit-hooks";
+    };
 
     # Easy packages
     pkgs-by-name.url = "github:drupol/pkgs-by-name-for-flake-parts";
 
     # Run checks automagically
     pre-commit-hooks.url = "github:cachix/git-hooks.nix";
-
-    # Zen browser
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    pre-commit-hooks.inputs = {
+      flake-compat.follows = "flake-compat";
+      nixpkgs.follows = "nixpkgs";
+      nixpkgs-stable.follows = "nixpkgs";
+    };
   };
 }

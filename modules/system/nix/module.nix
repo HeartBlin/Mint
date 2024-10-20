bb487228769716dc9a4a71279c4228ccbdaf8759
HeartBlin <heartblin@gmail.com>
2024-10-20 23:53:53 +0300

    fix: action should not complain about pipe-operator anymore

--- modules/system/nix/module.nix
+++ modules/system/nix/module.nix
@@ -64,25 +64,15 @@ in {
       builders-use-substitutes = true;
       substituters = [
         "https://cache.nixos.org?priority=10" # Funny
-        "https://cache.ngi0.nixos.org/" # CA nix
         "https://nix-community.cachix.org" # Community
-        "https://nixpkgs-unfree.cachix.org" # Unfree 1
-        "https://numtide.cachix.org" # Unfree 2
-        "https://nixpkgs-wayland.cachix.org" # Wayland
         "https://hyprland.cachix.org" # Hyprland
-        "https://nyx.cachix.org" # Foot-transparent
       ];
 
       # Keys for caches
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "nyx.cachix.org-1:xH6G0MO9PrpeGe7mHBtj1WbNzmnXr7jId2mCiq6hipE="
       ];
     };
   };

d831d02f2a3300d228eb185b9f5aea5f7223e599
HeartBlin <heartblin@gmail.com>
2024-10-23 16:25:46 +0300

    fix: Install needed programs for ags

--- modules/home/ags/module.nix
+++ modules/home/ags/module.nix
@@ -1,8 +1,18 @@
-{ config, inputs, ... }:
+{ config, inputs, inputs', ... }:
 
 let cfg = config.Mint.ags;
 in {
   imports = [ inputs.ags.homeManagerModules.default ];
 
-  programs.ags = { inherit (cfg) enable; };
+  programs.ags = {
+    inherit (cfg) enable;
+
+    extraPackages = with inputs'.ags.packages; [
+      battery
+      hyprland
+      network
+      tray
+      wireplumber
+    ];
+  };
 }

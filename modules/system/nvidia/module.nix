d7e1ea4573fd67115f3543726668770192978e59
HeartBlin <heartblin@gmail.com>
2024-10-23 21:50:10 +0300

    feat: Bump NVIDIA driver

--- modules/system/nvidia/module.nix
+++ modules/system/nvidia/module.nix
@@ -3,6 +3,15 @@
 let
   inherit (lib) mkDefault mkIf;
   cfg = config.Mint.nvidia;
+
+  bleedingEdge = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "565.57.01";
+    sha256_64bit = "sha256-buvpTlheOF6IBPWnQVLfQUiHv4GcwhvZW3Ks0PsYLHo=";
+    sha256_aarch64 = "sha256-aDVc3sNTG4O3y+vKW87mw+i9AqXCY29GVqEIUlsvYfE=";
+    openSha256 = "sha256-/tM3n9huz1MTE6KKtTCBglBMBGGL/GOHi5ZSUag4zXA=";
+    settingsSha256 = "sha256-H7uEe34LdmUFcMcS6bz7sbpYhg9zPCb/5AmZZFTx1QA=";
+    persistencedSha256 = "sha256-hdszsACWNqkCh8G4VBNitDT85gk9gJe1BlQ8LdrYIkg=";
+  };
 in {
   config = mkIf cfg.enable {
     nixpkgs.config = {
@@ -33,7 +42,7 @@ in {
     ];
 
     hardware.nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = bleedingEdge;
 
       dynamicBoost.enable = true;
       modesetting.enable = true;

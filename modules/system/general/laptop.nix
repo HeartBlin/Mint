083a80b989ccbf339123e69b7a1d9a0b7901e19c
HeartBlin <heartblin@gmail.com>
2024-11-08 16:51:36 +0000

    feat: Change governor if plugged in

--- modules/system/general/laptop.nix
+++ modules/system/general/laptop.nix
@@ -3,12 +3,25 @@
 let inherit (lib) mkForce mkIf;
 in {
   config = mkIf (role == "laptop") {
-    services.upower = {
-      enable = true;
-      percentageLow = 15;
-      percentageCritical = 5;
-      percentageAction = 2;
-      criticalPowerAction = "Hibernate";
+    services = {
+      auto-cpufreq = {
+        enable = true;
+        settings = {
+          charger.governor = "performance";
+          battery = {
+            governor = "powersave";
+            turbo = "never";
+          };
+        };
+      };
+
+      upower = {
+        enable = true;
+        percentageLow = 15;
+        percentageCritical = 5;
+        percentageAction = 2;
+        criticalPowerAction = "Hibernate";
+      };
     };
 
     hardware.bluetooth.powerOnBoot = mkForce false;

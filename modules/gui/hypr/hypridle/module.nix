5fb4935a7b7b8527b3aa2b1b745d9a8e7e3fb2a1
HeartBlin <heartblin@gmail.com>
2024-11-13 21:45:24 +0000

    feat: Disable hyrpidle on power

--- modules/gui/hypr/hypridle/module.nix
+++ modules/gui/hypr/hypridle/module.nix
@@ -46,11 +46,35 @@ let
   '';
 in {
   config = mkIf cfg.enable {
+    homix.".config/hypr/hypridle.conf".text = config';
     services.hypridle = {
       enable = true;
       package = inputs'.hypridle.packages.hypridle;
     };
 
-    homix.".config/hypr/hypridle.conf".text = config';
+    systemd.user.services.power-handler = {
+      description = "Handle power source changes for hypridle";
+      wantedBy = [ "default.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+        RemainAfterExit = "yes";
+        ExecStart = pkgs.writeShellScript "check-power" ''
+          while true; do
+            adp0_online=$(cat /sys/class/power_supply/ADP0/online)
+            usbc_online=$(cat /sys/class/power_supply/ucsi-source-psy-USBC000:001/online)
+
+            if [ "$adp0_online" = "1" ] || [ "$usbc_online" = "1" ]; then
+              systemctl --user stop hypridle
+            else
+              systemctl --user start hypridle
+            fi
+            sleep 2
+          done
+        '';
+        Restart = "always";
+        RestartSec = "5s";
+      };
+    };
   };
 }

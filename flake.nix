5def7f52393531b3fbd708f9317b3f5a90079c90
HeartBlin <heartblin@gmail.com>
2024-12-04 10:56:56 +0000

    feat: Switch to lix-master

--- flake.nix
+++ flake.nix
@@ -83,12 +83,19 @@
     };
 
     # Fork of Nix
-    lix = {
-      url =
-        "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
+    lix-master = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
     };
 
+    lix = {
+      url =
+        "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix-master";
+    };
+
+    # Nix language server
     nixd = {
       url = "github:/nix-community/nixd";
       inputs = {
